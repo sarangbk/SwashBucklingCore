51e895936c0d4f4d7bcd3d1ab2eb363d63cfa8b4
Sarang Kulkarni <sarangbk@gmail.com>
2016-04-20 03:38:32 +0530

    Updated the code to include an actual Article model.

--- src/SwashBucklingCore/Controllers/ArticlesController.cs
+++ src/SwashBucklingCore/Controllers/ArticlesController.cs
@@ -4,26 +4,33 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc;
 using Swashbuckle.SwaggerGen.Annotations;
+using SwashBucklingCore.Models;
  
 namespace SwashBucklingCore.Controllers
 {
     [Route("api/[controller]")]
     public class ArticlesController : Controller
     {
+        public List<Article> Articles { get; set; }
+        ArticlesController()
+        {
+            Articles.Add(new Article { Id = 1, Title = "Article1", Body ="This is an interestig article", Author = "Shakespeare"});
+            Articles.Add(new Article { Id = 2, Title = "Article2", Body ="This is another interestig article", Author = "Sarang"});
+        }
         // GET: api/articles
         [HttpGet]
         [SwaggerOperation("GetArticles")]
-        public IEnumerable<string> Get()
+        public IEnumerable<Article> Get()
         {
-            return new string[] { "Article1", "Article2" };
+            return Articles.ToArray();
         }
 
         // GET api/articles/5
         [HttpGet("{id}")]
         [SwaggerOperation("GetArticle")]
-        public string Get(int id)
+        public Article Get(int id)
         {
-            return "Article";
+            return Articles.Where(a => a.Id == id).FirstOrDefault();
         }
 
         // POST api/articles
---
+++ src/SwashBucklingCore/Models/Article.cs
@@ -0,0 +1,12 @@
+namespace SwashBucklingCore.Models
+{
+        public class Article
+        {
+            public int Id { get; set; }
+            public string Title { get; set; }
+            public string ByLine { get; set; }
+            public string Author { get; set; }
+            public string Body { get; set; }
+            
+        }
+}
